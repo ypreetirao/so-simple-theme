50a74387aaecf3167ad3a4ff1e24bd1c639a0e5d
Y Ramesh Rao <ryeduvakula@paypal.com>
2020-04-22 10:10:17 +0530

    GH Pages Build Failures fix

---
+++ cleanup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+rm -rvf ./.github
+rm -rvf ./docs
+rm -rvf ./example
+rm -rvf ./.editorconfig
+rm -rvf ./.gitattributes
+rm -rvf ./banner.js
+rm -rvf ./CHANGELOG.md
+rm -rvf ./Gemfile
+rm -rvf ./jekyll-theme-so-simple.gemspec
+rm -rvf ./package.json
+rm -rvf ./Rakefile
+rm -rvf ./README.md
+rm -rvf ./README-OLD.md
+rm -rvf ./screenshot.png
